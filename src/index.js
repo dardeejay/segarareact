9e300deef9c03e7c1e77b7a1febbdc7188be4164
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2022-07-15 00:59:58 +0800

    NavBar Done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
