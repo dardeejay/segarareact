578ee6cf1b5fe2c6c21c9cf5aa90db251c5881bc
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2022-07-16 00:36:48 +0800

    events page update

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import events_Background from "../assets/Events.png";
+import "../styles/events.scss";
 class Events extends Component {
   state = {};
   style = {
     backgroundImage: `url(${events_Background})`,
-    height: "500px",
-    width: "100%",
-    backgroundSize: "cover",
   };
   render() {
     return (
@@ -14,15 +12,37 @@ class Events extends Component {
         <div className="events_frontPage ">
           <div className="events_frontPage_container" style={this.style}>
             <div className="events_frontPage_container_text">
-              <h1>Events</h1>
+              <h1>#SegaraExperience</h1>
               <p></p>
             </div>
           </div>
         </div>
-
+        <div className="events_divider1">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 text-center mb-5">
+                <h1>MAKE MEMORIES</h1>
+                <h5>―</h5>
+                <h3>Celebrate what's important to you at our function room</h3>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="events_container">
           <div className="event1">
-            <div className="event1_images"></div>
+            <div className="event1_container">
+              <h1>Weddings</h1>
+            </div>
+          </div>
+          <div className="event2">
+            <div className="event2_container">
+              <h1>Birthdays</h1>
+            </div>
+          </div>
+          <div className="event3">
+            <div className="event3_container">
+              <h1>Corporate Events</h1>
+            </div>
           </div>
         </div>
       </div>
