79f3d92cc8b5cdc8757a7ef49ea48e1b6d92daba
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2022-07-15 22:43:14 +0800

    small update

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-
+import events_Background from "../assets/Events.png";
 class Events extends Component {
   state = {};
+  style = {
+    backgroundImage: `url(${events_Background})`,
+    height: "500px",
+    width: "100%",
+    backgroundSize: "cover",
+  };
   render() {
     return (
       <div>
-        <h1>Events</h1>
+        <div className="events_frontPage ">
+          <div className="events_frontPage_container" style={this.style}>
+            <div className="events_frontPage_container_text">
+              <h1>Events</h1>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="events_container">
+          <div className="event1">
+            <div className="event1_images"></div>
+          </div>
+        </div>
       </div>
     );
   }
