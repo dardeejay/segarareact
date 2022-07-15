c5929e9dce03166c4081add7b47ac408f2279533
Amrll <202110934@gordoncollege.edu.ph>
2022-07-16 02:29:04 +0800

    about us

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
-
+import about_us_Background from "../assets/About_Us.jpg";
+import "../styles/about-us.scss";
 class AboutUs extends Component {
   state = {};
+  style = {
+    backgroundImage: `url(${about_us_Background})`,
+  };
+
   render() {
     return (
       <div>
-        <h1>About Us</h1>
+
+      <div>
+        <div className="about_us">
+          <div className="about_us_container" style={this.style}>
+            <div className="about_us_container_text">
+              <h1>KNOW MORE ABOUT US</h1>
+              <h4>Segara Villas, Suites & Residencias</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
       </div>
     );
   }
