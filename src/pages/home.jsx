a656529c7085a73f9edc59dd142c70a5e83f70be
Amrll <202110934@gordoncollege.edu.ph>
2022-07-19 21:56:46 +0800

    Home and Rooms Update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,6 +12,12 @@ import HomeSlide1 from "../assets/HomeSwipe/1.jpg";
 import HomeSlide2 from "../assets/HomeSwipe/2.jpg";
 import HomeSlide3 from "../assets/HomeSwipe/3.jpg";
 import HomeSlide4 from "../assets/HomeSwipe/4.jpg";
+import HomeGallery1 from "../assets/HomeGallery/1.jpg";
+import HomeGallery2 from "../assets/HomeGallery/2.jpg";
+import HomeGallery3 from "../assets/HomeGallery/3.jpg";
+import HomeGallery4 from "../assets/HomeGallery/4.jpg";
+import HomeGallery5 from "../assets/HomeGallery/5.jpg";
+import HomeGallery6 from "../assets/HomeGallery/6.jpg";
 
 class Home extends Component {
   state = {};
@@ -45,33 +51,98 @@ class Home extends Component {
           </div>
         </div>
         {/* Swiper */}
-        <Swiper
-          modules={[EffectFade, Pagination, Navigation, Autoplay]}
-          effect="fade"
-          autoplay={(true, { disableOnInteraction: false })}
-          speed={800}
-          slidesPerView={1}
-          navigation={true}
-          pagination={{ clickable: true }}
-          loop
-          className="HomeSwiper"
-        >
-          <SwiperSlide className="HomeSwiperSlide">
-            <img src={HomeSlide1} alt="" className="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={HomeSlide2} alt="" className="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={HomeSlide3} alt="" className="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={HomeSlide4} alt="" className="" />
-          </SwiperSlide>
-        </Swiper>
-        {/* Swiper */}
-        <section id="home" className="banner_wrapper p-0"></section>
-
+        <div className="HomeSwiper-container">
+          <Swiper
+            modules={[EffectFade, Pagination, Navigation, Autoplay]}
+            effect="fade"
+            autoplay={(true, { disableOnInteraction: false })}
+            speed={800}
+            slidesPerView={1}
+            navigation={true}
+            pagination={{ clickable: true }}
+            loop
+            className="HomeSwiper"
+          >
+            <SwiperSlide className="HomeSwiperSlide">
+              <img src={HomeSlide1} alt="" className="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={HomeSlide2} alt="" className="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={HomeSlide3} alt="" className="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={HomeSlide4} alt="" className="" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        {/* Divider */}
+        <div className="divider">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center mb-5">
+                <h1>Asian Spices Bar and Restaurant</h1>
+                <h5>―</h5>
+                <p>
+                  An elegant and earthy spot for casual chic dining in the Subic
+                  Bay area. Tickle and gratify your taste buds with delectable
+                  Indonesian, Thai, Filipino, and Malaysian cuisines. Using only
+                  the best garden produce and ingredients, spices, herbs, herbs
+                  and authentic seasoning.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Resto */}
+        <section id="gallery" class="gallery_wrapper">
+          <div class="container">
+            <div class="row g-0">
+              <div class="col-lg-3 col-sm-3 gallery-item">
+                <img src={HomeGallery1} class="img-fluid w-100" />
+                <div class="gallery-item-content">
+                  <img src={HomeGallery2} class="img-fluid w-100" />
+                </div>
+              </div>
+              <div class="col-lg-3 col-sm-3 gallery-item">
+                <img src={HomeGallery2} class="img-fluid w-100" />
+                <div class="gallery-item-content">
+                  <img src={HomeGallery4} class="img-fluid w-100" />
+                </div>
+              </div>
+              <div class="col-lg-3 col-sm-3 gallery-item">
+                <img src={HomeGallery3} class="img-fluid w-100" />
+                <div class="gallery-item-content"></div>
+              </div>
+              <div class="col-lg-3 col-sm-3 gallery-item">
+                <img src={HomeGallery4} class="img-fluid w-100" />
+                <div class="gallery-item-content"></div>
+              </div>
+              <div class="col-sm-6 gallery-item">
+                <img src={HomeGallery3} class="img-fluid w-100" />
+                <div class="gallery-item-content"> </div>
+              </div>
+              <div class="col-sm-6 gallery-item">
+                <img src={HomeGallery3} class="img-fluid w-100" />
+                <div class="gallery-item-content">
+                  <img src={HomeGallery3} class="img-fluid w-100" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Divider */}
+        <div className="divider">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center mb-5">
+                <h1>Know More About Segara Hotels</h1>
+                <h5>―</h5>
+              </div>
+            </div>
+          </div>
+        </div>
         {/*
         <section id="home" className="banner_wrapper p-0">
           <div className="HomeContainer">
--- src/pages/rooms&rates.jsx
+++ src/pages/rooms&rates.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { motion } from "framer-motion";
+import "../styles/rooms&rates.scss";
+import VillasImg from "../assets/Villas.jpg";
+import SuitesImg from "../assets/Suites.jpg";
+import ResidenciasImg from "../assets/Residencias.jpg";
 
 class RoomsAndRates extends Component {
   state = {};
@@ -10,7 +14,160 @@ class RoomsAndRates extends Component {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
-        <h1>Rooms & Rates</h1>
+        {/*FrontPage*/}
+        <div className="RoomsandRates_frontPage container-fluid">
+          <div className="row">
+            <div className="RoomsandRates_frontPage_container">
+              <div className="RoomsandRates_frontPage_container_text">
+                <h1 className="display-1">LUXURIOUS STAY</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Divider */}
+        <div className="divider">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center mb-5">
+                <h3>A TRULY LUXURIOUS UNFORGETTABLE STAY</h3>
+                <h5>―</h5>
+                <h1>VILLAS • SUITES • RESIDENCIAS</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*Rooms Container*/}
+        <div>
+          <div className="RoomsandRatesMain container-fluid">
+            <div className="RoomsandRatesMain_container row flex-lg-row">
+              <div className="col-md-6 sec1_text order-sm-2 order-lg-1">
+                <div className="row gy-0">
+                  <div className="col-md-6 p-5">
+                    <h1>Villas</h1>
+                  </div>
+                  <div className="col-md-12 px-5">
+                    <p>
+                      A grandiose abode inside Subic Bay that offers 10
+                      luxurious villas for a peaceful accommodation. An
+                      undisturbed boutique hotel that provides superb
+                      personalized service and tremendous amenities.
+                    </p>
+                  </div>
+                </div>
+                <div className="col-md-12 px-5 text-center">
+                  <a className="main-btn-rooms" href="/">
+                    Book a Villa
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-6 text-center order-sm-1">
+                <img
+                  src={VillasImg}
+                  alt="anniversary package"
+                  className="Roomsimg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="RoomsandRatesMain container-fluid">
+            <div className="RoomsandRatesMain_container row flex-lg-row">
+              <div className="col-lg-6 text-center order-sm-1">
+                <img
+                  src={SuitesImg}
+                  alt="anniversary package"
+                  className="Roomsimg"
+                />
+              </div>
+              <div className="col-md-6 sec1_text order-sm-2 order-lg-1">
+                <div className="row gy-0">
+                  <div className="col-md-6 p-5">
+                    <h1>Suites</h1>
+                  </div>
+                  <div className="col-md-12 px-5">
+                    <p>
+                      An 18-room luxury hotel for all types of travelers. Our
+                      spacious rooms, pampering amenities and friendly services
+                      will surely make your stay memorable and truly
+                      revitalizing.
+                    </p>
+                  </div>
+                </div>
+                <div className="col-md-12 px-5 text-center">
+                  <a className="main-btn-rooms" href="/">
+                    Book a Suite
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="RoomsandRatesMain container-fluid">
+            <div className="RoomsandRatesMain_container row flex-lg-row">
+              <div className="col-md-6 sec1_text order-sm-2 order-lg-1">
+                <div className="row gy-0">
+                  <div className="col-md-6 p-5">
+                    <h1>Residencias</h1>
+                  </div>
+                  <div className="col-md-12 px-5">
+                    <p>
+                      The newest Asian-Mexican themed hotel and condo in Subic
+                      Bay. Offering more than 40 rooms, Segara Residencias is
+                      very ideal for families, friends, and other big groups.
+                    </p>
+                  </div>
+                </div>
+                <div className="col-md-12 px-5 text-center">
+                  <a className="main-btn-rooms" href="/">
+                    Book a Residencia
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-6 text-center order-sm-1">
+                <img
+                  src={ResidenciasImg}
+                  alt="anniversary package"
+                  className="Roomsimg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Divider */}
+        <div className="divider">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center mb-5">
+                <h3>LUXURIOUS SERVICES FROM SEGARA HOTELS</h3>
+                <h5>―</h5>
+                <h1>ROOM FACILITIES</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* amenities */}
+        <div className="amenities container-fluid">
+          <div className="ameneties row flex-lg-row">
+            <div className="col-sm-4 text-center">
+              <h3>Pool</h3>
+              <h3>Free parking</h3>
+              <h3>Outdoor dining area</h3>
+              <h3>Free breakfast</h3>
+            </div>
+            <div className="col-sm-4 text-center">
+              <h3>King size luxury bed</h3>
+              <h3>Wooden flooring</h3>
+              <h3>Hot and cold water</h3>
+              <h3>Laundry service</h3>
+            </div>
+            <div className="col-sm-4 text-center">
+              <h3>Large balcony</h3>
+              <h3>Free Wi-Fi</h3>
+              <h3>Air conditioning</h3>
+              <h3>Non-smoking hotel</h3>
+            </div>
+          </div>
+        </div>
+        {/* Bottom page Images */}
+
       </motion.div>
     );
   }
