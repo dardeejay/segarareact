2f42389a361be35ed2cef85557cbe76ff820a2f8
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2022-07-19 12:42:42 +0800

    messenger update ulit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './pages/navbar'
-
+import Messenger from './pages/messenger'
 import AnimatedPage from './pages/AnimatedPage';
 function App() {
   return (
     <Router>
       <NavBar />
         <AnimatedPage/>
+      <Messenger />
     </Router>
   );
 }
---
+++ src/pages/messenger.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
+function Messenger() {
+  return (
+    <div>
+      <MessengerCustomerChat
+        pageId="103455685784435
+        "
+        appId="787123462284202"
+      />
+      ,
+    </div>
+  );
+}
+
+export default Messenger;
