9311341947b12493bd43af733f6b733346b274b8
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2022-07-21 18:58:18 +0800

    Ready for presentation na

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -17,18 +17,16 @@ function Footer() {
       </div>
 
       <div className="container  footer-text">
-        <div className="row justify-content-between">
+        <div className="row justify-content-between text-center text-lg-start">
           <div className="col-12 col-lg-4">
             <h3>Our address</h3>
             <h5>Segara Hotels Subic</h5>
             <div className="row">
-              <div className="col-1">
-                <FontAwesomeIcon icon="fa-solid fa-location-dot" />
-              </div>
-              <div className="col-11">
+              <div className="col-12">
                 <p>
-                  Segara Suites Main Building, Washington St, Subic Bay Freeport
-                  Zone, Zambales
+                  <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+                  &nbsp; Segara Suites Main Building, Washington St, Subic Bay
+                  Freeport Zone, Zambales
                 </p>
               </div>
             </div>
@@ -37,21 +35,28 @@ function Footer() {
             <h3>Contact Us</h3>
             <h5>For Rooms and Bookings</h5>
             <div className="row g-0 mb-2">
-              <div className="col-1">
-                <FontAwesomeIcon icon="fa-solid fa-phone" />
+              <div className="col-12">
+                <p>
+                  <FontAwesomeIcon icon="fa-solid fa-phone" />
+                  &nbsp; 09088142438
+                </p>
               </div>
-              <div className="col-11">09088142438</div>
-              <div className="col-1">
-                <FontAwesomeIcon icon="fa-solid fa-envelope" />
+
+              <div className="col-12">
+                <p>
+                  <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                  &nbsp; reservations@subicsegaravillas.com
+                </p>
               </div>
-              <div className="col-11">reservations@subicsegaravillas.com</div>
             </div>
             <h5>For Events</h5>
             <div className="row mb-5">
-              <div className="col-1">
-                <FontAwesomeIcon icon="fa-solid fa-phone" />
+              <div className="col-12">
+                <p>
+                  <FontAwesomeIcon icon="fa-solid fa-phone" />
+                  &nbsp; 09177955965
+                </p>
               </div>
-              <div className="col-11">09177955965</div>
             </div>
           </div>
           <div className="col-12 col-lg-4">
@@ -59,37 +64,56 @@ function Footer() {
           </div>
         </div>
       </div>
-      {/* <div className="footer-end">
+      <div className="footer-end">
         <div className="container">
-          <div className="row justify-content-between">
-            <div className="col-3">
+          <div className="row justify-content-between text-center text-lg-start">
+            <div className="col-lg-3 col-sm-12">
               <p>© Copyright Segara Hotels</p>
             </div>
-            <div className="col-3">
+            <div className="col-lg-3 col-sm-12">
               <div className="row text-center justify-content-center">
-                <div className="col-12">Follow Us</div>
-                <div className="col-2">
-                  <p>
-                    <FontAwesomeIcon icon="fa-brands fa-facebook-square fa-lg" />
-                  </p>
+                <div className="col-12">
+                  <p>Follow Us</p>
                 </div>
-                <div className="col-2">
-                  <p>
-                    {" "}
+                <div className="col-2 mt-1">
+                  <a
+                    href="https://www.facebook.com/SegaraHotels/"
+                    style={{ color: "black" }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon icon="fa-brands fa-facebook-square fa-lg" />
+                  </a>
+                </div>
+                <div className="col-2 mt-1">
+                  <a
+                    style={{ color: "black" }}
+                    href="https://www.instagram.com/segarahotels/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FontAwesomeIcon icon="fa-brands fa-instagram fa-lg" />
-                  </p>
+                  </a>
                 </div>
                 <div className="col-2 ">
-                  <img src={tripAdvisorLogo} alt="" className="img-fluid" />
+                  <a
+                    href="https://www.tripadvisor.com.ph/Hotel_Review-g4751237-d4176944-Reviews-Segara_Suites-Subic_Bay_Freeport_Zone_Bataan_Province_Central_Luzon_Region_Luzon.html"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={tripAdvisorLogo} alt="" className="img-fluid" />
+                  </a>
                 </div>
               </div>
             </div>
-            <div className="col-3">
-              <p>Contact Us Terms and Condition</p>
+            <div className="col-lg-3 col-sm-12 terms-and-condition d-flex justify-content-around">
+              <p>Contact Us</p>
+
+              <p>Terms and Condition</p>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
